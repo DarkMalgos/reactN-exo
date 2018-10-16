1a14574ae137110bbb9646adb6d6ed911a04c5ba
Florian SAUMUROT <florian.saumurot@gmail.com>
2018-10-16 11:59:56 +0200

    Last version with reusable component

---
+++ exo1/src/components/card.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+type Props = {};
+
+export default class Card extends Component<Props> {
+    render() {
+        return(
+            <View style={styles.containerStyle}>
+                {this.props.children}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        borderColor: '#ddd',
+        borderWidth: 1,
+        borderRadius: 2,
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: {widt: 0, height: 2},
+        shadowOpacity: .1,
+        shadowRadius: 2,
+        elevation: 2,
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5
+    }
+});

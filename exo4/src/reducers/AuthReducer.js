ce35d91d33d1b813438d809db79498f2f964fcb1
Florian SAUMUROT <florian.saumurot@gmail.com>
2018-10-17 17:56:48 +0200

    some redux action for log a manager

---
+++ exo4/src/reducers/AuthReducer.js
@@ -0,0 +1,11 @@
+import { EMAIL_CHANGED } from '../actions/types';
+
+const INTIAL_STATE = { email: ''};
+export default (state = INTIAL_STATE, action) => {
+    switch (action.type) {
+        case EMAIL_CHANGED:
+            break;
+        default:
+            return state;
+    }
+}
